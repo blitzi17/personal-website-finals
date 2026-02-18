ebace7a09f01b6e08de12eb8687d0d200c01ba27
Karol Joy Ronquillo <167147436+blitzi17@users.noreply.github.com>
2026-02-18 06:32:16 +0000

    change code

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import GuestbookView from '../views/GuestbookView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/guestbook',
+      name: 'guestbook',
+      component: GuestbookView
+    }
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return { el: to.hash, behavior: 'smooth' }
+    }
+    return savedPosition || { top: 0 }
+  }
 })
 
 export default router
