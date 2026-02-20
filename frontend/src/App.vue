c9504ff86c7cef7c727bedf30b31275d24cf1aa6
Karol Joy Ronquillo <167147436+blitzi17@users.noreply.github.com>
2026-02-20 15:28:05 +0000

    replace app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,9 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <NavBar />
+  <RouterView />
 </template>
 
-<style scoped></style>
+<script setup>
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+</script>
